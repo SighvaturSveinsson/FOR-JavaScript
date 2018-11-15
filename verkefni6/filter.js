7109cf73a27e860627dd89af1f3e6e67dbf43703
SighvaturSveinsson <sighvatursv373@nemi.tskoli.is>
2018-11-15 12:07:46 +0000

    Add files via upload

--- verkefni6/filter.js
+++ verkefni6/filter.js
@@ -1,10 +1,22 @@
 (function() {
 	let search = document.getElementById('filter-search');
 	let tags = document.getElementById('buttons');
-	let images = document.getElementsByTagName('img');
+	let images = [];
+	//let images = document.getElementById('buttons');
+	let gallery = document.getElementById('gallery');
+	let imgAlts = ["Rabbit", "Sea", "Deer"]
+	let imgTags = ["Animators", "Designers", "Photographers","Filmmakers", "Illustrators"]
+
+
+	for (i = 0; i < imgAlts.length; i++){
+		var myImage = new Image();
+		myImage.src = 'img/p'+(i+1)+'.jpg';
+		myImage.alt = imgAlts[i];
+		images.push(myImage);
+		gallery.appendChild(myImage);
+	}
 	/*
 	let cache = [];
-
 	for(i = 0; i<images.length; i++){
 		cache.push(images[i].alt);
 	}
@@ -21,5 +33,5 @@
 	}
 
 	search.addEventListener("keyup", searchFilter);
-	//tags.addEventListener("onclick", searchFilter);
+	//tags.addEventListener("onclick", tagFilter);
 }());
